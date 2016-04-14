112490b2d097285e9ab9cb459ab28fd6129b1ef8
Matthias Isler <matthias.isler@smartbridge.ch>
2016-04-14 18:41:02 +0200

    added files back

---
+++ src/Spiritix/LadaCache/Database/Model.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * This file is part of the spiritix/lada-cache package.
+ *
+ * @copyright Copyright (c) Matthias Isler <mi@matthias-isler.ch>
+ * @license   MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Spiritix\LadaCache\Database;
+
+use Illuminate\Database\Eloquent\Model as EloquentModel;
+
+/**
+ * Overrides Laravel's model class.
+ *
+ * If Laravel would not have hardcoded the query builder class, this file would not be required anymore.
+ * It would also not be required to have all models extending this class.
+ *
+ * @package Spiritix\LadaCache\Database
+ * @author  Matthias Isler <mi@matthias-isler.ch>
+ */
+class Model extends EloquentModel
+{
+    /**
+     * Get a new query builder instance for the connection.
+     *
+     * @return \Illuminate\Database\Query\Builder
+     */
+    protected function newBaseQueryBuilder()
+    {
+        $conn = $this->getConnection();
+        $grammar = $conn->getQueryGrammar();
+
+        return new QueryBuilder($conn, $grammar, $conn->getPostProcessor());
+    }
+}
